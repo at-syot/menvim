1d031969149e41d106ae1a9401a91ba0d07b5d54
at-syot <aotsincere@gmail.com>
2023-10-17 23:36:54 -0400

    add keymap for curr buffer references

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,7 +5,7 @@ lsp_zero.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr}
 
   vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, {})
   vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
@@ -22,7 +22,8 @@ require('mason-lspconfig').setup({
 	  "gopls", 
 	  "svelte",
 	  "html",
-	  "tailwindcss"
+	  "tailwindcss",
+    "volar"
   },
   handlers = {
     lsp_zero.default_setup,
@@ -39,6 +40,7 @@ lsp_zero.format_on_save({
 		['lua'] = {'lua'},
     ['tsserver'] = {'javascript', 'typescript'},
 		['svelte'] = {'svelte'},
+    ['volar'] = {'vue'},
 		['gopls'] = {'go'}
 	}
 })
