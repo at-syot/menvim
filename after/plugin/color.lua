7b60ccd50f3b5b6585765c6cc83fd0563dfe3d79
at-syot <aotsincere@gmail.com>
2023-10-21 12:28:40 -0400

    remove rose-pine colorscheme

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,12 +1,8 @@
-require('rose-pine').setup({
-  disable_float_background = true,
-	disable_italics = true,
-})
-
 function ColorMyPencils(color) 
 	-- color = color or 'rose-pine'
 	-- vim.cmd.colorscheme(color)
   color = color or 'melange'
+  vim.g.melang_enable_font_variants = 0
   vim.cmd { cmd = 'set', args = {'background=dark'} };
   vim.cmd.colorscheme(color)
 end
